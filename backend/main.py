b8973f93a35c14b816a8692558985159f256ccd0
pmongeholgado <pmongeholgado@gmail.com>
2025-11-22 21:09:08 +0100

    main

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,18 @@
 from flask import Flask, send_from_directory
 import os
 
-app = Flask(__name__, static_folder='.')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SITIO_DIR = os.path.join(BASE_DIR, "sitio")
+
+app = Flask(__name__, static_folder=SITIO_DIR)
 
 @app.route('/')
 def index():
-    return send_from_directory('sitio', 'index.html')
+    return send_from_directory(SITIO_DIR, 'index.html')
 
-@app.route('/<path:ruta>')
-def archivos(ruta):
-    return send_from_directory('.', ruta)
+@app.route('/<path:archivo>')
+def archivos(archivo):
+    return send_from_directory(SITIO_DIR, archivo)
 
 if __name__ == '__main__':
     puerto = int(os.environ.get('PORT', 8000))
