8f73ba77df3ea2de2804b663e3f427a772490aec
pmongeholgado <pmongeholgado@gmail.com>
2025-12-02 18:23:51 +0100

    Update main.py

--- programanova_backend/main.py
+++ programanova_backend/main.py
@@ -48,10 +48,3 @@ def chat():
 
     return jsonify(response)
 
-
-# =====================================
-# RUN SERVER
-# =====================================
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
