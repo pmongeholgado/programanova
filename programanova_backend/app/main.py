ad09f2f4d671f5ff666674294f904d598c23ec56
pmongeholgado <pmongeholgado@gmail.com>
2025-12-03 20:44:05 +0100

    Update main.py

--- programanova_backend/app/main.py
+++ programanova_backend/app/main.py
@@ -1,20 +1,9 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-# Crear la app de Flask
 app = Flask(__name__)
 CORS(app)
 
-# Ruta principal (/)
-@app.route("/", methods=["GET"])
-def home():
-    return jsonify({
-        "status": "ok",
-        "message": "API Programanova funcionando",
-        "autor": "Nova & Pablo"
-    })
-
-# Ruta de salud (/status)
 @app.route("/status", methods=["GET"])
 def status():
     return jsonify({
