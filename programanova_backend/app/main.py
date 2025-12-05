5e9090f8798f122dd6ce2f68167578aa8ceec8c2
pmongeholgado <pmongeholgado@gmail.com>
2025-12-05 14:34:31 +0100

    Update main.py

--- programanova_backend/app/main.py
+++ programanova_backend/app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 # ================================
 # INIT FLASK
@@ -60,6 +61,6 @@ def chat():
     return jsonify(respuesta), 200
 
 
-# Solo se usa si ejecutaras localmente con `python main.py`
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
