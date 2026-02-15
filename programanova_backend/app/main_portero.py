ac352763405620da8f70739cf091ede72bd488a8
pmongeholgado <pmongeholgado@gmail.com>
2026-02-15 20:09:46 +0100

    Update main_portero.py

--- programanova_backend/app/main_portero.py
+++ programanova_backend/app/main_portero.py
@@ -1,7 +1,7 @@
 # main_portero.py
 # Arranque seguro: envuelve la app existente SIN modificarla
 
-from main import app  # <- importa tu app actual (NO se toca main.py)
+from app.main_pro import app  # 👈 IMPORT REAL SEGÚN TU DOCKERFILE
 
 from nova_portero.middleware_portero import PorteroMiddleware
 from nova_portero.config_portero import PORTERO_CONFIG
